ae335497df740204fda5768b262c9d1ad4f93fd0
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-07 14:53:40 -0400

    3.1

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,57 @@
+import { z } from "zod";
+import type { Actions, PageServerLoad } from "./$types";
+import { setError, superValidate } from "sveltekit-superforms/server";
+import { fail } from "@sveltejs/kit";
+
+const registerUserSchema = z.object({
+	full_name: z.string().max(140, "Name must be 140 characters or less").nullish(),
+	email: z.string().email("Invalid email address"),
+	password: z
+		.string()
+		.min(6, "Password must be at least 6 characters")
+		.max(64, "Password must be 64 characters or less"),
+	passwordConfirm: z
+		.string()
+		.min(6, "Password must be at least 6 characters")
+		.max(64, "Password must be 64 characters or less")
+});
+
+export const load: PageServerLoad = async (event) => {
+	return {
+		form: superValidate(registerUserSchema)
+	};
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		const form = await superValidate(event, registerUserSchema);
+
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		if (form.data.password !== form.data.passwordConfirm) {
+			return setError(form, "passwordConfirm", "Passwords do not match");
+		}
+
+		const { error: authError } = await event.locals.supabase.auth.signUp({
+			email: form.data.email,
+			password: form.data.password,
+			options: {
+				data: {
+					full_name: form.data.full_name ?? ""
+				}
+			}
+		});
+
+		if (authError) {
+			return setError(form, null, "An error occurred while registering.");
+		}
+
+		return {
+			form
+		};
+	}
+};
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import { Card, Button } from "flowbite-svelte";
+	import { superForm } from "sveltekit-superforms/client";
+	import type { PageData } from "./$types";
+	export let data: PageData;
+
+	const { form, errors, enhance } = superForm(data.form, {
+		resetForm: true
+	});
 </script>
 
 <div class="py-20">
@@ -8,22 +15,34 @@
 			<h1 class="text-4xl font-semibold">Register for an account</h1>
 		</div>
 		<Card class="mt-6 w-full" padding="xl" size="md">
-			<form class="flex flex-col space-y-6">
+			<form method="POST" class="flex flex-col space-y-6" use:enhance>
 				<label class="space-y-2" for="full_name">
 					<span>Name</span>
-					<input type="text" name="full_name" />
+					<input type="text" name="full_name" bind:value={$form.full_name} />
+					{#if $errors.full_name}
+						<span class="block text-red-600 dark:text-red-500">{$errors.full_name}</span>
+					{/if}
 				</label>
 				<label class="space-y-2" for="email">
 					<span>Email</span>
-					<input type="email" name="email" />
+					<input type="email" name="email" bind:value={$form.email} />
+					{#if $errors.email}
+						<span class="block text-red-600 dark:text-red-500">{$errors.email}</span>
+					{/if}
 				</label>
 				<label class="space-y-2" for="password">
 					<span>Password</span>
-					<input type="password" name="password" />
+					<input type="password" name="password" bind:value={$form.password} />
+					{#if $errors.password}
+						<span class="block text-red-600 dark:text-red-500">{$errors.password}</span>
+					{/if}
 				</label>
 				<label class="space-y-2" for="passwordConfirm">
 					<span>Confirm Password</span>
-					<input type="password" name="passwordConfirm" />
+					<input type="password" name="passwordConfirm" bind:value={$form.passwordConfirm} />
+					{#if $errors.passwordConfirm}
+						<span class="block text-red-600 dark:text-red-500">{$errors.passwordConfirm}</span>
+					{/if}
 				</label>
 				<Button type="submit" class="w-full">Register</Button>
 				<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
