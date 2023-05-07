9240484d3f1781514c66b6ba38d931305af008ce
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-07 18:28:47 -0400

    4.8

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,10 +1,18 @@
-import { clearSupabaseData, createUser, startSupabase } from "./utils";
+import { clearSupabaseData, createContact, createUser, startSupabase } from "./utils";
 
 async function seed() {
 	try {
 		await startSupabase();
 		await clearSupabaseData();
-		await createUser({ email: "t@t.com", full_name: "Test user", password: "password" });
+		const user = await createUser({
+			email: "t@t.com",
+			full_name: "Test user",
+			password: "password"
+		});
+
+		for (let i = 0; i < 5; i++) {
+			await createContact(user.id);
+		}
 	} catch (err) {
 		console.error(err);
 		process.exit(1);
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -2,10 +2,10 @@ import detect from "detect-port";
 import { execSync } from "child_process";
 import pg from "pg";
 import { ENV } from "$lib/server/env";
-import { createClient } from "@supabase/supabase-js";
-import type { Database } from "$lib/supabase-types";
 import type { z } from "zod";
 import type { registerUserSchema } from "$lib/schemas";
+import { supabaseAdmin } from "$lib/server/supabase-admin";
+import { faker } from "@faker-js/faker";
 
 export async function startSupabase() {
 	const port = await detect(54322);
@@ -24,12 +24,10 @@ export async function clearSupabaseData() {
 	await client.query("TRUNCATE auth.users CASCADE");
 }
 
-const supabase = createClient<Database>(ENV.PUBLIC_SUPABASE_URL, ENV.PUBLIC_SUPABASE_ANON_KEY);
-
 type CreateUser = Omit<z.infer<typeof registerUserSchema>, "passwordConfirm">;
 
 export async function createUser(user: CreateUser) {
-	const { data: authData, error: authError } = await supabase.auth.signUp({
+	const { data: authData, error: authError } = await supabaseAdmin.auth.signUp({
 		email: user.email,
 		password: user.password,
 		options: {
@@ -43,4 +41,24 @@ export async function createUser(user: CreateUser) {
 		throw new Error("Error creating user");
 	}
 	return authData.user;
+}
+
+export async function createContact(user_id: string) {
+	const firstName = faker.name.firstName();
+	const lastName = faker.name.lastName();
+	const contact = {
+		name: `${firstName} ${lastName}`,
+		email: faker.internet.exampleEmail(firstName, lastName),
+		company: faker.company.name(),
+		phone: faker.phone.number(),
+		user_id
+	};
+
+	const { error, data } = await supabaseAdmin.from("contacts").insert(contact);
+
+	if (error) {
+		throw error;
+	}
+
+	return data;
 }
