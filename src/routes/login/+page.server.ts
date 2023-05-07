4c26e4a5532cef68705ec82dac64772b42ccb5b2
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-07 14:59:30 -0400

    3.2

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { setError, superValidate } from "sveltekit-superforms/server";
+import type { Actions, PageServerLoad } from "./$types";
+import { fail, redirect } from "@sveltejs/kit";
+import { AuthApiError } from "@supabase/supabase-js";
+
+const loginUserSchema = z.object({
+	email: z.string().email("Please enter a valid email address"),
+	password: z.string().min(1, "Please enter a password")
+});
+
+export const load: PageServerLoad = async (event) => {
+	return {
+		form: superValidate(loginUserSchema)
+	};
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		const form = await superValidate(event, loginUserSchema);
+
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		const { error: authError } = await event.locals.supabase.auth.signInWithPassword(form.data);
+
+		if (authError) {
+			if (authError instanceof AuthApiError && authError.status === 400) {
+				setError(form, "email", "Invalid credentials");
+				setError(form, "password", "Invalid credentials");
+				return fail(400, {
+					form
+				});
+			}
+		}
+
+		throw redirect(302, "/");
+	}
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import { Card, Button } from "flowbite-svelte";
+	import { superForm } from "sveltekit-superforms/client";
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+	const { form, errors, enhance } = superForm(data.form);
 </script>
 
 <div class="py-20">
@@ -10,14 +15,20 @@
 		</div>
 		<Card class="mt-6 w-full" padding="xl" size="md">
 			<!-- Login Form -->
-			<form class="flex flex-col space-y-6">
+			<form method="POST" class="flex flex-col space-y-6" use:enhance>
 				<label class="space-y-2" for="email">
 					<span>Email</span>
-					<input type="email" name="email" />
+					<input type="email" name="email" bind:value={$form.email} />
+					{#if $errors.email}
+						<span class="block text-red-600 dark:text-red-500">{$errors.email}</span>
+					{/if}
 				</label>
 				<label class="space-y-2" for="password">
 					<span>Password</span>
-					<input type="password" name="password" />
+					<input type="password" name="password" bind:value={$form.password} />
+					{#if $errors.password}
+						<span class="block text-red-600 dark:text-red-500">{$errors.password}</span>
+					{/if}
 				</label>
 				<Button type="submit" class="w-full">Login</Button>
 				<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
