51dc5f5754b8e3d2dc53d47255ff22037d355012
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-07 21:55:04 -0400

    9.3

--- src/routes/api/stripe/checkout/+server.ts
+++ src/routes/api/stripe/checkout/+server.ts
@@ -41,7 +41,12 @@ export const GET: RequestHandler = async (event) => {
 				metadata: {
 					user_id: session.user.id
 				},
-				trial_period_days: 14
+				trial_period_days: 14,
+				trial_settings: {
+					end_behavior: {
+						missing_payment_method: "cancel"
+					}
+				}
 			},
 			payment_method_collection: "if_required"
 		});
