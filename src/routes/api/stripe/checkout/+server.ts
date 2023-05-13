31928755334143ae03e464bd3f1db84ceaa028ad
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-12 22:38:23 -0400

    add tier check for checkout

--- src/routes/api/stripe/checkout/+server.ts
+++ src/routes/api/stripe/checkout/+server.ts
@@ -1,12 +1,18 @@
 import { error, redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
-import { createCheckoutSession } from "$lib/server/subscriptions";
+import { createCheckoutSession, getSubscriptionTier } from "$lib/server/subscriptions";
 import { handleLoginRedirect } from "$lib/helpers";
 
 export const GET: RequestHandler = async (event) => {
 	const session = await event.locals.getSession();
 	if (!session) {
 		throw redirect(302, handleLoginRedirect(event));
+	}
+
+	const tier = await getSubscriptionTier(session.user.id);
+
+	if (tier !== "Free") {
+		throw redirect(302, "/account/billing");
 	}
 
 	const price_id = event.url.searchParams.get("id");
