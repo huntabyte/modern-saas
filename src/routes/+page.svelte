7ff4108d4418007d470d48cb735295ebecb7d887
mastmagan007 <tshanajk@gmail.com>
2023-11-15 03:05:42 +0500

    Before experiment of editable website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { Button } from "flowbite-svelte";
+	import { useChat } from 'ai/svelte'
+
+	const { messages, input,handleSubmit } = useChat();
 </script>
 
 <div class="py-20">
@@ -14,4 +17,31 @@
 		<Button href="/pricing" size="lg">Get Started</Button>
 		<Button href="/pricing" color="light" size="lg">Learn More</Button>
 	</div>
+
+	<section class="text-center mt-16">
+		<h2 class="mb-5">Use Chat</h2>
+		<ul>
+			{#each $messages as m}
+				<li>  {m.role === 'user' ? 'User: ' : 'AI: '}: {m.content}</li>
+			{/each}
+		</ul>
+		<form on:submit={handleSubmit} class="flex">
+			<input class="border border-lime-700" type="text" name="email" bind:value={$input} data-testid="email" />
+
+			<Button class="bg-lime-700 px-3 -ms-2" type="submit">Send</Button>
+		</form>
+	</section>
 </div>
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
---
+++ src/routes/api/chat-with-functions/+server.ts
@@ -0,0 +1,72 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import type { CompletionCreateParams } from 'openai/resources/chat';
+
+import { env } from '$env/dynamic/private';
+
+const openai = new OpenAI({
+  apiKey: env.OPENAI_API_KEY || '',
+});
+
+const functions: CompletionCreateParams.Function[] = [
+  {
+    name: 'get_current_weather',
+    description: 'Get the current weather',
+    parameters: {
+      type: 'object',
+      properties: {
+        location: {
+          type: 'string',
+          description: 'The city and state, e.g. San Francisco, CA',
+        },
+        format: {
+          type: 'string',
+          enum: ['celsius', 'fahrenheit'],
+          description:
+            'The temperature unit to use. Infer this from the users location.',
+        },
+      },
+      required: ['location', 'format'],
+    },
+  },
+  {
+    name: 'get_current_time',
+    description: 'Get the current time',
+    parameters: {
+      type: 'object',
+      properties: {},
+      required: [],
+    },
+  },
+  {
+    name: 'eval_code_in_browser',
+    description: 'Execute javascript code in the browser with eval().',
+    parameters: {
+      type: 'object',
+      properties: {
+        code: {
+          type: 'string',
+          description: `Javascript code that will be directly executed via eval(). Do not use backticks in your response.
+           DO NOT include any newlines in your response, and be sure to provide only valid JSON when providing the arguments object.
+           The output of the eval() will be returned directly by the function.`,
+        },
+      },
+      required: ['code'],
+    },
+  },
+];
+
+export async function POST({ request }) {
+  const { messages, function_call } = await request.json();
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-0613',
+    stream: true,
+    messages,
+    functions,
+    function_call,
+  });
+
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
