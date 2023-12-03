edcff5425dc1576f5afdf255f1a981f36f079fe4
mastmagan007 <tshanajk@gmail.com>
2023-12-04 03:58:35 +0500

    api key masla

--- src/routes/api/getListings/+server.ts
+++ src/routes/api/getListings/+server.ts
@@ -1,9 +1,9 @@
-import {sparkAPI} from "$env/static/private"
+import {SPARK_API_KEY} from "$env/static/private"
 export const GET = async ({ request, url }) => {
 
   const res = await fetch('https://replication.sparkapi.com/v1/listings',{
     headers: {
-      Authorization: `Bearer ${sparkAPI}`,
+      Authorization: `Bearer ${SPARK_API_KEY}`,
       'Content-Type': 'application/json',
     },
   });
