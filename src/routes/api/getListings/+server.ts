9c00faa56addcbb72f72f72ecd7a54bec407726e
Taimoor Shan <tshanajk@gmail.com>
2023-12-04 04:19:13 +0500

    Update +server.ts

--- src/routes/api/getListings/+server.ts
+++ src/routes/api/getListings/+server.ts
@@ -1,14 +1,14 @@
-import {SPARK_API_KEY} from "$env/static/private"
-export const GET = async ({ request, url }) => {
+// import {SPARK_API_KEY} from "$env/static/private"
+// export const GET = async ({ request, url }) => {
 
-  const res = await fetch('https://replication.sparkapi.com/v1/listings',{
-    headers: {
-      Authorization: `Bearer ${SPARK_API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  const data = await res.json()
+//   const res = await fetch('https://replication.sparkapi.com/v1/listings',{
+//     headers: {
+//       Authorization: `Bearer ${SPARK_API_KEY}`,
+//       'Content-Type': 'application/json',
+//     },
+//   });
+//   const data = await res.json()
 
   
-  return new Response(JSON.stringify(data), { status: 200 })
-}
+//   return new Response(JSON.stringify(data), { status: 200 })
+// }
